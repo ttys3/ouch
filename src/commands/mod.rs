bbc9e604159d79115a040e916592ea2950d214e1
ttyS3 <ttys3.rust@gmail.com>
2024-03-24 03:47:20 +0000

    feat: add password support for decompress and list

--- src/commands/mod.rs
+++ src/commands/mod.rs
@@ -182,6 +182,7 @@ pub async fn run(
                         output_file_path,
                         question_policy,
                         args.quiet,
+                        args.password.as_deref(),
                     )
                 })?;
         }
@@ -215,7 +216,7 @@ pub async fn run(
                     println!();
                 }
                 let formats = extension::flatten_compression_formats(&formats);
-                list_archive_contents(archive_path, formats, list_options, question_policy)?;
+                list_archive_contents(archive_path, formats, list_options, question_policy, args.password.as_deref())?;
             }
         }
     }
